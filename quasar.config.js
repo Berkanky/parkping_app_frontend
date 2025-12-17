6b8eb3f050cec17a7c48fe0ffee71b885f6f02a8
Berkan <yusufbk92@gmail.com>
2025-12-18 00:11:47 +0300

    fix

--- quasar.config.js
+++ quasar.config.js
@@ -41,6 +41,11 @@ export default defineConfig((/* ctx */) => {
       },
 
       vueRouterMode: 'hash', // available values: 'hash', 'history'
+        env: {
+          QUASAR_PUBLIC_GOOGLE_CLIENT_ID: process.env.QUASAR_PUBLIC_GOOGLE_CLIENT_ID,
+          QUASAR_PUBLIC_BACKEND_URL: process.env.QUASAR_PUBLIC_BACKEND_URL,
+          VITE_WEB_SOCKET_API_URL: process.env.VITE_WEB_SOCKET_API_URL
+        }
       // vueRouterBase,
       // vueDevtools,
       // vueOptionsAPI: false,
